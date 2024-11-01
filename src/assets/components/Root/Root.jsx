306b78c130f99b08c58f6c78eab01eded11f1694
Samadsust71 <samadsust71@gmail.com>
2024-11-01 20:55:28 +0600

    initial setup done

---
+++ src/assets/components/Root/Root.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+
+const Root = () => {
+  return (
+    <div className='w-11/12 mx-auto mt-6'>
+     <NavBar/>
+    </div>
+  )
+}
+
+export default Root
